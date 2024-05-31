b6331beb9a6362b0c9917e82c75e146f916e4b93
ThomasLaspas <makislaspas@gmail.com>
2024-05-31 21:01:48 +0300

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["pgiyjcnrihxvwrkmcyux.supabase.co"],
+    domains: ["mjrpazptfzmpupxuchsv.supabase.co"],
   },
 };
 
